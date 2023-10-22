d3e722d717810fae4c5de17b040405600fa3168c
StopRefactoring <yiigaa@126.com>
2023-10-22 17:08:48 +0800

    [update]read version info from version.txt

--- Christmas.py
+++ Christmas.py
@@ -10,25 +10,34 @@
 import sys
 sys.dont_write_bytecode = True
 from Kernel.Move.Move import Move
+from Kernel.Config.Config import Config
+import os
+
+def PrintVersion():
+    with open('./version.txt', 'r') as file:
+        content = file.read()
+    print(content)
 
 if __name__ == '__main__':
-    print('Hi, I am Christmas v2.0')
-    print('More Detail: https://github.com/YiiGaa/Christmas')
-    print('official: https://stoprefactoring.com')
-    print('')
-
     if not (sys.version_info.major >= 3 and sys.version_info.minor >= 9):
         print('Error! Require python 3.9 or above.')
         print('Current: '+sys.version)
         exit(-1)
     
     if len(sys.argv)>1:
+        rootPath = sys.argv[0]
+        if rootPath != "Christmas.py" and rootPath != "./Christmas.py":
+            rootPath = rootPath.replace('Christmas.py','')
+            Config.inputPath = rootPath + Config.inputPath
+            os.chdir(rootPath)
+        PrintVersion()
         action = sys.argv[1]
         setting = []
         for list in sys.argv[2:]:
             setting.append(list)
         Move.Start(action, setting)
     else:
+        PrintVersion()
         print("Please input function parameters.")
         print("Like: python3 Christmas.py Input/GenCode/xxx")
         print("Input/GenCode/xxx is function parameters.")
