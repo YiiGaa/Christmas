5c1877dd0aa0501fc20a20737192d3078a7b841a
StopRefactoring <38609414+YiiGaa@users.noreply.github.com>
2026-04-14 20:57:46 +0800

    [update]Sync Christmas3

---
+++ Kernel/Move/GenCode/GenCode.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+from Kernel.Module.Module import Module
+from Kernel.Common.Logger.Logger import Logger
+
+class GenCode:
+    def DoStart(targetParam, configParam):
+        print('STEP::Read Template')
+        targetParam = Module.Start('GetTemplate', targetParam, {
+            'mod_templateKey':'Xmas_templ',
+            'mod_templatePath':configParam['templatePath']
+        })
+
+        print('STEP::Recursive Processing')
+        targetParam = Module.Start('ReplaceTemplate', targetParam, {
+            'mod_templateKey':'Xmas_templ'
+        })
+
+        print('STEP::Write File')
+        targetParam = Module.Start('WriteFile', targetParam, {
+            'mod_fileKey':'Xmas_path',
+            'mod_isJudgeExist':configParam['isJudgeExist'],
+            'mod_isExistBackup':configParam['isExistBackup'],
+            'mod_isFillLack':configParam['isFillLack'],
+            'mod_fillLackMap':configParam['fillLackMap']
+        })
+
+    def Start(targetParam, configParam):
+        GenCode.DoStart(targetParam, configParam)
+        print('')
+        Logger.Tips('SUCCESS')
---
+++ Kernel/Move/ShellExcute/ShellExcute.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+from Kernel.Module.Module import Module
+from Kernel.Common.Logger.Logger import Logger
+
+class ShellExcute:
+    def DoStart(targetParam, configParam):
+        print('STEP::Excute Shell Command')
+        targetParam = Module.Start('ShellExcute', targetParam, {
+            'mod_commandKey':'Xmas_shell',
+            'mod_cwdKey':'Xmas_cwd',
+            'mod_judgeKey':'Xmas_judge',
+            'mod_errorKey':'Xmas_error',
+            "mod_inputExtraKey":'Xmas_'
+        })
+
+    def Start(targetParam, configParam):
+        ShellExcute.DoStart(targetParam, configParam)
+        print('')
+        Logger.Tips('SUCCESS')
